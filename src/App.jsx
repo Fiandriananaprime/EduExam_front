393b0ea31087656b627cea3a0699fe6ac6f38b64
Fiandriananaprime <hei.fiandrianana@gmail.com>
2026-08-22 16:49:39 +0300

    fix: pass role prop to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import { RouterProvider } from "react-router-dom";
-<<<<<<< HEAD
 import router from "./Route";
-=======
-import router from "./router";
->>>>>>> eceb4e667478fe51496d989e331ebecfc3198325
 import './App.css';
 
 const App = () => {
