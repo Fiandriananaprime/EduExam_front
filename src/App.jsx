2d04dbfddedf26e12d58826cf5b47fab7237882d
Fiandriananaprime <hei.fiandrianana@gmail.com>
2026-08-22 14:46:32 +0300

    feat: implement protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import './App.css'
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+import './App.css';
 
- const App = () => {
-  return null;
-}
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
