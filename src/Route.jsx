79f78d67d7d96c24b69fe2ad387346557774d83d
Fiandriananaprime <hei.fiandrianana@gmail.com>
2026-08-25 19:48:43 +0300

    feat(routing): integrate protected role-based routes

---
+++ src/Route.jsx
@@ -0,0 +1,98 @@
+import { createBrowserRouter, Navigate } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Unauthorized from "./pages/Unauthorized";
+
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import AdminStudents from "./pages/admin/AdminStudents";
+import AdminCourses from "./pages/admin/AdminCourses";
+import AdminExams from "./pages/admin/AdminExams";
+import AdminQuestions from "./pages/admin/AdminQuestions";
+import AdminExamResults from "./pages/admin/AdminExamResults";
+
+import StudentExams from "./pages/student/StudentExams";
+import StudentExam from "./pages/student/StudentExam";
+import StudentExamResult from "./pages/student/StudentExamResult";
+import StudentResults from "./pages/student/StudentResults";
+
+import ProtectedRoute from "./security/ProtectedRoute";
+import Layout from "./components/Layout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/login" replace />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    element: <ProtectedRoute allowedRoles={"ADMIN"} />,
+    children:[
+      {
+        element:<Layout />,
+        children: [
+          {
+            path: "/admin",
+            element: <AdminDashboard />,
+          },
+          {
+            path: "/admin/students",
+            element: <AdminStudents />,
+          },
+          {
+            path: "/admin/courses",
+            element: <AdminCourses />,
+          },
+          {
+            path: "/admin/exams",
+            element: <AdminExams />,
+          },
+          {
+            path: "/admin/exams/:id/questions",
+            element: <AdminQuestions />,
+          },
+          {
+            path: "/admin/exams/:id/results",
+            element: <AdminExamResults />,
+          },
+        ],
+      }
+    ]
+  },
+
+  {
+    element: <ProtectedRoute allowedRoles={"STUDENT"} />,
+    children: [
+      {
+        element:<Layout />,
+        children: [
+          {
+            path: "/student",
+            element: <StudentExams />,
+          },
+          {
+            path: "/student/exams/:id/result",
+            element: <StudentExamResult />,
+          },
+          {
+            path: "/student/results",
+            element: <StudentResults />,
+          },
+        ],
+      },
+      {
+      path: "/student/exams/:id",
+      element: <StudentExam />,
+    },
+    ]
+  },
+
+  {
+    path: "/unauthorized",
+    element: <Unauthorized />,
+  },
+]);
+
+export default router;
